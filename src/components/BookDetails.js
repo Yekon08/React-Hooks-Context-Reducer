e43a75bde0018793b92d917acac10e2da76ee287
Yekon08 <max1901@live.fr>
2020-04-07 11:09:57 +0200

    add reducer

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -2,9 +2,9 @@ import React, { useContext } from 'react'
 import { BookContext } from '../contexts/BookContext'
 
 const BookDetails = (props) => {
-    const { removeBook } = useContext(BookContext)
+    const { dispatch } = useContext(BookContext)
     return (
-        <li onClick={() => removeBook(props.book.id)}>
+        <li onClick={() => dispatch({type: 'REMOVE_BOOK', id: props.book.id})}>
             <div className='title'>{ props.book.title }</div>
             <div className='author'>{ props.book.author }</div>
         </li>
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,23 +1,21 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useReducer } from 'react'
+import { bookReducer } from '../reducers/bookReducer'
 
 export const BookContext = createContext()
 
 export const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        {title: 'Books number one', author: 'Bob', id: 1},
-        {title: 'Books number two', author: 'Paul', id: 2}
-    ])
+    const [books, dispatch] = useReducer( bookReducer, [] )
 
-    const addBook = (title, author) => {
-        setBooks([...books, {title: title, author: author, id: books.length+1}])
-    }
+    // const addBook = (title, author) => {
+    //     setBooks([...books, {title: title, author: author, id: books.length+1}])
+    // }
 
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id))
-    }
+    // const removeBook = (id) => {
+    //     setBooks(books.filter(book => book.id !== id))
+    // }
 
     return (
-        <BookContext.Provider value={{books, addBook, removeBook}}>
+        <BookContext.Provider value={{books, dispatch}}>
             {props.children}
         </BookContext.Provider>
     )
